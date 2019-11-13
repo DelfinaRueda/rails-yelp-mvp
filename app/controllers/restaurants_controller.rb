9d66c1009c9a8f11dcf34196043a40c341ad70b3
Delfina Rueda <delfinarueda@gmail.com>
2019-11-13 16:47:59 +0000

    Made a version of the restaurants app that contains multiple reviews that can be added to the restaurant

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,33 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        format.html { render :new }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:      'chinese',
+    phone_number:   '6196157735'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '6196157735'
+  },
+  {
+    name:         'tea',
+    address:      'Paris',
+    category:      'belgian',
+    phone_number:   '6196157735'
+  },
+  {
+    name:         'diavolo',
+    address:      'Buenos Aires',
+    category:      'french',
+    phone_number:  '6196157735'
+  },
+  {
+    name:         'leon',
+    address:      'Paseo conde de los Gaitanes',
+    category:      'japanese',
+    phone_number:   '6196157735'
+
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
